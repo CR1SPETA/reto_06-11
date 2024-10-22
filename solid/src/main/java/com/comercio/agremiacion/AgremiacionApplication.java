465089694ae4cb5a7d5ea95574fb009c35d5a156
CR1SPETA <arcano507@hotmail.com>
2024-10-22 17:12:21 -0500

    incial commit
    inicial commit

---
+++ solid/src/main/java/com/comercio/agremiacion/AgremiacionApplication.java
@@ -0,0 +1,13 @@
+package com.comercio.agremiacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgremiacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgremiacionApplication.class, args);
+	}
+
+}
---
+++ solid/src/test/java/com/comercio/agremiacion/AgremiacionApplicationTests.java
@@ -0,0 +1,14 @@
+package com.comercio.agremiacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgremiacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
